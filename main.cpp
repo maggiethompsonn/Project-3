a767363691f2bf094d31140a54b24b7f478679d4
maggiethompsonn <92807359+maggiethompsonn@users.noreply.github.com>
2022-12-05 14:26:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,68 +23,75 @@ int main() {
 
     cout << "Which factors are most important to you? (rank in order ex: 23145)" << endl;
     cout << "1. Genre \n2. Author \n3. Number of Pages \n4. Award-Winning \n5. Publish Date \n";
-    int bookRank = 0;
-    cin >> bookRank;
-    int firstChoice = 0;
-    int secondChoice = 0;
-    int thirdChoice = 0;
-    int fourthChoice = 0;
-    int fifthChoice = 0;
-    for(int i = 5; i > 0; i--)
-    {
-        int temp = bookRank / pow(10, 5 - i) % 10;
-        if(i == 1)
-        {
-            firstChoice = temp;
-        }
-        if(i == 2)
-        {
-            secondChoice = temp;
-        }
-        if(i == 3)
-        {
-            thirdChoice = temp;
-        }
-        if(i == 4)
-        {
-            fourthChoice = temp;
-        }
-        if(i == 5)
-        {
-            fifthChoice = temp;
-        }
-    }
 
-    int genre = -1;
-    string author = "";
-    int pageRange = -1;
-    bool awardWinning = 0;
-    int publishDate = -1;
+
     //4. based on their ranking, ask for their choices for each option
     //   and perform the associated method to add points to matching books
     //   maybe use a for loop here to iterate through their ranking in order
-    int numPoints = 30; //start at 30, decrement by 5 with each iteration through loop
+    //  start at 30, decrement by 5 with each iteration through loop
+    //  genre: ask for the genre they are looking for
+    //  author: ask for the author they are looking for
+    //  num pages: ask for a page range
+    //  award-winning: ask if they care if the book has won awards or not
+    //  published date: give ranges on year dates and ask them to select their range
 
-        //genre: ask for the genre they are looking for
-    cout << "Please type a number for the genre you are looking for." << endl;
-    cout << ; //List all genres in numbered list
-    cin >> genre;
-        //author: ask for the author they are looking for
-    cout << "Please type the name of the author you are looking for (only first and last name)" << endl;
-    cin >> author;
-        //num pages: ask for a page range
-    cout << "Please type a number for the page range you are looking for." << endl;
-    cout << ; //List all page ranges in numbered list
-    cin >> pageRange;
-        //award-winning: ask if they care if the book has won awards or not
-    cout << "If reading award winning books are important to you, please type '1', otherwise type '0'." << endl;
-    cin >> awardWinning;
-        //published date: give ranges on year dates and ask them to select their range
-    cout << "Please type a number for a published date range." << endl;
-    cout << ; //List published date ranges in numbered list
-    cin >> publishDate;
+    string factorsRank, genreRank, authorRank, awardWinning, pageRank, publishDate;
+    bool awardWinningBool;
+    int numPoints = 30;
+
+    cin >> factorsRank;
+    for (int i = 0; i < 5; i++) {
+        if (factorsRank[i] == '1') {
+            cout << "Choose your top 3 genres and rank them in order (Ex: Romance, Fiction, Historical)" << endl;
+            cout << "Adult, Adventure, Childrens, Classics, Drama, Dystopia, Fantasy, Fiction, \n"
+                    "Historical Fiction, Horror, Humor, Literature, Mystery, Romance, \n"
+                    "Science Fiction, Spiritual, Supernatural, Teen, Thriller, Young Adult \n";
+            cin >> genreRank;
+            books.genrePoints(genreRank, numPoints);
+        }
+        if (factorsRank[i] == '2') {
+            cout << "Enter in the first and last name of your top 2 authors separated by a comma.\n"
+                    "(Ex: Suzanne Collins, J.K. Rowling)" << endl;
+            cin >> authorRank;
+            books.authorPoints(authorRank, numPoints);
+        }
+        if (factorsRank[i] == '3') {
+            cout << "Select the page range you are looking for, or no preference. (Ex: 2)\n"
+                    "1. 100-200 \n"
+                    "2. 200-300 \n"
+                    "3. 300-400 \n"
+                    "4. 400-500 \n"
+                    "5. 500+\n"
+                    "6. No preference" << endl;
+            cin >> pageRank;
+            books.numPagesPoints(pageRank, numPoints);
+        }
+        if (factorsRank[i] == '4') {
+            cout << "Is it important for your book selection to be an award winning book? (Ex: yes or no)" << endl;
+            cin >> awardWinning;
+            if (awardWinning == "yes")
+                awardWinningBool = true;
+            else
+                awardWinningBool = false;
+            books.awardWinningPoints(awardWinningBool, numPoints);
+        }
+        if (factorsRank[i] == '5') {
+            cout << "Select your desired range of how long ago the book was published, or no preference. (Ex: 5)\n"
+                    "1. <5 years ago \n"
+                    "2. 5-10 years\n"
+                    "3. 10-20 years \n"
+                    "4. 20+ years\n"
+                    "5. No preference" << endl;
+            cin >> publishDate;
+            books.publishDatePoints(publishDate, numPoints);
+        }
+        numPoints = numPoints-5;
+
+    }
 
     //5. return a list of the top 10 books for the user
+
+
 
 
     return 0;
