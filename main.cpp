5c45eb904e49a8781b97cf5e2bf92010a472bc71
r3noble <92817123+r3noble@users.noreply.github.com>
2022-12-05 14:16:42 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -32,7 +32,7 @@ int main() {
     int fifthChoice = 0;
     for(int i = 5; i > 0; i--)
     {
-        int temp = bookRank / pow(10, i - 5) % 10;
+        int temp = bookRank / pow(10, 5 - i) % 10;
         if(i == 1)
         {
             firstChoice = temp;
@@ -55,16 +55,34 @@ int main() {
         }
     }
 
+    int genre = -1;
+    string author = "";
+    int pageRange = -1;
+    bool awardWinning = 0;
+    int publishDate = -1;
     //4. based on their ranking, ask for their choices for each option
     //   and perform the associated method to add points to matching books
     //   maybe use a for loop here to iterate through their ranking in order
     int numPoints = 30; //start at 30, decrement by 5 with each iteration through loop
 
         //genre: ask for the genre they are looking for
+    cout << "Please type a number for the genre you are looking for." << endl;
+    cout << ; //List all genres in numbered list
+    cin >> genre;
         //author: ask for the author they are looking for
+    cout << "Please type the name of the author you are looking for (only first and last name)" << endl;
+    cin >> author;
         //num pages: ask for a page range
+    cout << "Please type a number for the page range you are looking for." << endl;
+    cout << ; //List all page ranges in numbered list
+    cin >> pageRange;
         //award-winning: ask if they care if the book has won awards or not
+    cout << "If reading award winning books are important to you, please type '1', otherwise type '0'." << endl;
+    cin >> awardWinning;
         //published date: give ranges on year dates and ask them to select their range
+    cout << "Please type a number for a published date range." << endl;
+    cout << ; //List published date ranges in numbered list
+    cin >> publishDate;
 
     //5. return a list of the top 10 books for the user
 
