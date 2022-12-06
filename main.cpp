20006049019370dcb4b701ea5b34762772088430
maggiethompsonn <92807359+maggiethompsonn@users.noreply.github.com>
2022-12-06 12:31:56 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,76 +14,68 @@ int main() {
     songs.readCSV("songs_good.csv");
     //songs.mergeSort(0, songs.listLength() - 1);
 
-    //2. welcome the user to the Book Worm's book recommender
-    cout << "Welcome to the Book Worm Book Recommender!" << endl;
+    //2. welcome the user to the song recommender
+    cout << "Welcome to the Song Recommender!" << endl;
     cout << "******************************************" << endl;
 
     // 3. ask the user to rank which criteria are most important to them
-    //    genre, author, num pages, award-winning, publisher, publish date
+    //    danceability, energy, loudness, mode, acousticness, valence, tempo
 
-    cout << "Which factors are most important to you? (rank in order ex: 23145)" << endl;
-    cout << "1. Genre \n2. Author \n3. Number of Pages \n4. Award-Winning \n5. Publish Date \n";
+    cout << "Which factors are most important to you? (rank in order ex: 2314576)" << endl;
+    cout << "1. Danceability \n2. Energy \n3. Loudness \n4. Mode \n5. Acousticness \n6. Valence \n 7. Tempo";
 
 
     //4. based on their ranking, ask for their choices for each option
     //   and perform the associated method to add points to matching books
     //   maybe use a for loop here to iterate through their ranking in order
     //  start at 30, decrement by 5 with each iteration through loop
-    //  genre: ask for the genre they are looking for
-    //  author: ask for the author they are looking for
-    //  num pages: ask for a page range
-    //  award-winning: ask if they care if the book has won awards or not
-    //  published date: give ranges on year dates and ask them to select their range
 
-    string factorsRank, genreRank, authorRank, awardWinning, pageRank, publishDate;
-    bool awardWinningBool;
+    string factorsRank, mode;
+    int danceRank, energyRank, loudnessRank, acousticRank, valenceRank, tempoRank;
     int numPoints = 30;
 
     cin >> factorsRank;
     for (int i = 0; i < 5; i++) {
         if (factorsRank[i] == '1') {
-            cout << "Choose your top 3 genres and rank them in order (Ex: Romance, Fiction, Historical)" << endl;
-            cout << "Adult, Adventure, Childrens, Classics, Drama, Dystopia, Fantasy, Fiction, \n"
-                    "Historical Fiction, Horror, Humor, Literature, Mystery, Romance, \n"
-                    "Science Fiction, Spiritual, Supernatural, Teen, Thriller, Young Adult \n";
-            cin >> genreRank;
-            books.genrePoints(genreRank, numPoints);
+            cout << "On a scale of 1-10, how much would you like to be able to dance to your songs? (1-no dancing, 10-lots of dancing)" << endl;
+            cin >> danceRank;
+            songs.dancePoints(danceRank, numPoints);
         }
         if (factorsRank[i] == '2') {
-            cout << "Enter in the first and last name of your top 2 authors separated by a comma.\n"
-                    "(Ex: Suzanne Collins, J.K. Rowling)" << endl;
-            cin >> authorRank;
-            books.authorPoints(authorRank, numPoints);
+            cout << "Energetic tracks typically feel fast, loud, and noisy. How much energy do you want in your songs on a scale of 1-10? (1-low energy, 10-high energy)"<< endl;
+            cin >> energyRank;
+            songs.energyPoints(energyRank, numPoints);
         }
         if (factorsRank[i] == '3') {
-            cout << "Select the page range you are looking for, or no preference. (Ex: 2)\n"
-                    "1. 100-200 \n"
-                    "2. 200-300 \n"
-                    "3. 300-400 \n"
-                    "4. 400-500 \n"
-                    "5. 500+\n"
-                    "6. No preference" << endl;
-            cin >> pageRank;
-            books.numPagesPoints(pageRank, numPoints);
+            cout << "How loud would you like your songs to be on a scale of 1-10? (Ex: 1-quiet, 10-very loud)" << endl;
+            cin >> loudnessRank;
+            songs.loudnessPoints(loudnessRank, numPoints);
         }
         if (factorsRank[i] == '4') {
-            cout << "Is it important for your book selection to be an award winning book? (Ex: yes or no)" << endl;
-            cin >> awardWinning;
-            if (awardWinning == "yes")
-                awardWinningBool = true;
-            else
-                awardWinningBool = false;
-            books.awardWinningPoints(awardWinningBool, numPoints);
+            cout << "Would you like your songs to be in the major or minor?" << endl;
+            cin >> mode;
+            songs.modePoints(mode, numPoints);
         }
         if (factorsRank[i] == '5') {
-            cout << "Select your desired range of how long ago the book was published, or no preference. (Ex: 5)\n"
-                    "1. <5 years ago \n"
-                    "2. 5-10 years\n"
-                    "3. 10-20 years \n"
-                    "4. 20+ years\n"
-                    "5. No preference" << endl;
-            cin >> publishDate;
-            books.publishDatePoints(publishDate, numPoints);
+            cout << "How acoustic would you like your songs to be on a scale of 1-10?" << endl;
+            cin >> acousticRank;
+            songs.acousticPoints(acousticRank, numPoints);
+        }
+        if (factorsRank[i] == '6') {
+            cout << "How positive would you like your songs to be on a scale of 1-10? (Ex: 1-sad, depressed, angry; 10-happy, cheerful, euphoric)" << endl;
+            cin >> valenceRank;
+            songs.valencePoints(valenceRank, numPoints);
+        }
+        if (factorsRank[i] == '7') {
+            cout << "How fast would you like the tempo of your songs to be?\n"
+                    "1. 0-70 BPM\n"
+                    "2. 70-100 BPM\n"
+                    "3. 100-130 BPM\n"
+                    "4. 130-160 BPM\n"
+                    "5. 160-190 BPM\n"
+                    "6. 190+ BPM" << endl;
+            cin >> tempoRank;
+            songs.tempoPoints(tempoRank, numPoints);
         }
         numPoints = numPoints-5;
     }
